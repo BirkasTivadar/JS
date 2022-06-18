cd8bcc4e68013b135f5a8e67737810802d7e4a17
BirkasTivadar <birkastivadar@gmail.com>
2022-06-18 18:31:20 +0200

    Modulok importálása más néven

--- modules/export/app.js
+++ modules/export/app.js
@@ -12,9 +12,25 @@ console.log(subtractValues(20, 10));
  */
 
 // 2. módszer
+/* 
 import { user, username, numArr, sum, subtract } from './module.js';
 console.log(user);
 console.log(username);
 console.log(numArr);
 console.log(sum(10, 15));
 console.log(subtract(20, 10));
+*/
+
+// Importálni is tudok más néven
+import {
+    user,
+    username,
+    numArr,
+    sumValues as sum,
+    subtractValues as sub
+} from './module.js';
+console.log(user);
+console.log(username);
+console.log(numArr);
+console.log(sum(10, 15));
+console.log(sub(20, 10));
--- modules/export/module.js
+++ modules/export/module.js
@@ -2,7 +2,7 @@
 
 // 1. módszer
 // ha kiteszem egy Object, variable, array, függvény, stb. elé az export kulcsszót, akkor más fájlokba be tudom importálni
-/* 
+
 export const user = {
     name: 'John Doe',
     age: 30
@@ -15,13 +15,14 @@ export const numArr = [1, 2, 3, 4, 5];
 const pi = 3.14 // amivel adott fájl-on belül dolgozom, de azon kívül nem, az nem kell exportálni
 export const sumValues = (x, y) => x + y * pi;
 export const subtractValues = (x, y) => x - y;
- */
+
 
 // 2. módszer
 /* 
 megtehetem, hogy a végén gyűjtöm össze, amiket exportálni akarok,
 DE nem lehet duplikáció (a kódon belül is kiteszem az export-ot, meg a végén is)
 */
+/* 
 const user = {
     name: 'John Doe',
     age: 30
@@ -42,3 +43,4 @@ export {
     sumValues as sum,
     subtractValues as subtract
 };
+ */
