fe264d9ace69829eeaa70a66f7c5730d6036856a
BirkasTivadar <birkastivadar@gmail.com>
2022-06-18 18:26:22 +0200

    Modulok exportálása más néven

--- modules/export/app.js
+++ modules/export/app.js
@@ -1,9 +1,20 @@
 'use strict';
+// Csak azt importáljuk, amire szükségünk van
 
+// 1. módszer
+/* 
 import { user, username, numArr, sumValues, subtractValues } from './module.js';
-
 console.log(user);
 console.log(username);
 console.log(numArr);
 console.log(sumValues(10, 15));
 console.log(subtractValues(20, 10));
+ */
+
+// 2. módszer
+import { user, username, numArr, sum, subtract } from './module.js';
+console.log(user);
+console.log(username);
+console.log(numArr);
+console.log(sum(10, 15));
+console.log(subtract(20, 10));
--- modules/export/module.js
+++ modules/export/module.js
@@ -1,6 +1,8 @@
 'use strict';
 
+// 1. módszer
 // ha kiteszem egy Object, variable, array, függvény, stb. elé az export kulcsszót, akkor más fájlokba be tudom importálni
+/* 
 export const user = {
     name: 'John Doe',
     age: 30
@@ -13,8 +15,30 @@ export const numArr = [1, 2, 3, 4, 5];
 const pi = 3.14 // amivel adott fájl-on belül dolgozom, de azon kívül nem, az nem kell exportálni
 export const sumValues = (x, y) => x + y * pi;
 export const subtractValues = (x, y) => x - y;
+ */
+
+// 2. módszer
 /* 
 megtehetem, hogy a végén gyűjtöm össze, amiket exportálni akarok,
 DE nem lehet duplikáció (a kódon belül is kiteszem az export-ot, meg a végén is)
- */
-// export { user, username, numArr, sumValues, subtractValues };
+*/
+const user = {
+    name: 'John Doe',
+    age: 30
+};
+
+const username = 'Jane Doe';
+
+const numArr = [1, 2, 3, 4, 5];
+
+const pi = 3.14 // amivel adott fájl-on belül dolgozom, de azon kívül nem, az nem kell exportálni
+const sumValues = (x, y) => x + y * pi;
+const subtractValues = (x, y) => x - y;
+// ez esetben akár más néven is tudom exportálni, de ez esetben ilyen néven kell importálni, és használni is
+export {
+    user,
+    username,
+    numArr,
+    sumValues as sum,
+    subtractValues as subtract
+};
